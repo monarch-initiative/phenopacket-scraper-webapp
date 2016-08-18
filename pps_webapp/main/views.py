8ffa81a36ebcd6f594957e24b1d8303e6c170e95
satwik77 <satwik55@gmail.com>
2016-08-18 19:15:20 +0530

    UI update

--- pps_webapp/main/views.py
+++ pps_webapp/main/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import get_object_or_404, render_to_response, redirect
 from django.shortcuts import render
 from django.contrib import auth
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 import requests
 
 api_url= 'http://localhost:8001/api/'
 
+@csrf_exempt
 def home(request):
     if request.POST:
         choice = str(request.POST['choice'])
@@ -16,9 +18,9 @@ def home(request):
             response = requests.get(api_url+ 'scrape', params = get_data)
             if response.status_code == 200:
                 response_data = response.json()
-                abstract = response_data['Abstract']
-                title = response_data['Title']
-                hpo_terms = response_data['HPO Terms']
+                abstract = str(response_data['Abstract'])
+                title = str(response_data['Title'])
+                hpo_terms = str(response_data['HPO Terms'])
                 data = data + [abstract, title, hpo_terms]
 
         if choice == '2':
@@ -26,8 +28,8 @@ def home(request):
             response = requests.get(api_url+ 'annotate', params = get_data)
             if response.status_code == 200:
                 response_data = response.json()
-                annotated_terms = response_data['Annotated HPO Terms']
-                annotated_abstract = response_data['Annotated Abstract']
+                annotated_terms = str(response_data['Annotated HPO Terms'])
+                annotated_abstract = str(response_data['Annotated Abstract'])
                 data = data + [annotated_abstract, annotated_terms]
 
         if choice == '3':
@@ -38,11 +40,8 @@ def home(request):
                 phenopacket = response_data['phenopacket']
                 data = [phenopacket]
 
-        context = {
-        'data' : data,
-        }
-
-        return render(request, 'main/home.html', context)
+        response = '\n'.join(data)
+        return HttpResponse(response)
 
     return render(request, 'main/home.html')
 
