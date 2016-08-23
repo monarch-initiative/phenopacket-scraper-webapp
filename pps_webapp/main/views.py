4382c2a4e501448e7bfd68c7826a3c4c5ab39a26
satwik77 <satwik55@gmail.com>
2016-08-23 22:54:20 +0530

    Output Formatting

--- pps_webapp/main/views.py
+++ pps_webapp/main/views.py
@@ -4,6 +4,7 @@ from django.contrib import auth
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import requests
+import pprint
 
 api_url= 'http://localhost:8001/api/'
 
@@ -12,25 +13,33 @@ def home(request):
     if request.POST:
         choice = str(request.POST['choice'])
         url = str(request.POST['url'])
-        data = []
+        data = ""
         if choice == '1':
             get_data={'url' : str(url)}
             response = requests.get(api_url+ 'scrape', params = get_data)
+
             if response.status_code == 200:
                 response_data = response.json()
-                abstract = str(response_data['Abstract'])
+                abstract = response_data['Abstract']
                 title = str(response_data['Title'])
-                hpo_terms = str(response_data['HPO Terms'])
-                data = data + [abstract, title, hpo_terms]
+                hpo_terms = response_data['HPO Terms']
+                data+= "Title:\n" + title + "\n"
+                data+="Abstract:\n" + abstract + "\n"
+                data+="HPO Terms:\n"
+                for term in hpo_terms:
+                    data += str(term) + "\n"
+
 
         if choice == '2':
             get_data={'url' : str(url)}
             response = requests.get(api_url+ 'annotate', params = get_data)
             if response.status_code == 200:
                 response_data = response.json()
-                annotated_terms = str(response_data['Annotated HPO Terms'])
-                annotated_abstract = str(response_data['Annotated Abstract'])
-                data = data + [annotated_abstract, annotated_terms]
+                data = {}
+                data["annotated_terms"] = response_data['Annotated HPO Terms']
+                data["annotated_abstract"] = response_data['Annotated Abstract']
+                data= pprint.pformat(data, indent=4)
+                
 
         if choice == '3':
             get_data={'url' : str(url)}
@@ -38,10 +47,9 @@ def home(request):
             if response.status_code == 200:
                 response_data = response.json()
                 phenopacket = response_data['phenopacket']
-                data = [phenopacket]
+                data = phenopacket
 
-        response = '\n'.join(data)
-        return HttpResponse(response)
+        return HttpResponse(data)
 
     return render(request, 'main/index.html')
 
